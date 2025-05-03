ace161bb425a1c92f9948cdd955edabe7e5081c7
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-05-03 04:48:24 -0300

    FUNDAMENTOS/render JSX elements in functions

--- Curso React/FUNDAMENTOS/event/src/components/on-click-event/OnClickEvent.jsx
+++ Curso React/FUNDAMENTOS/event/src/components/on-click-event/OnClickEvent.jsx
@@ -7,13 +7,29 @@ const OnClickEvent = (e) => {
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
+    if (clicked) {
+      setClicked(false);
+      return;
+    }
+
     setClicked(true);
   };
 
+  const renderText = () => {
+    return (
+      <p>
+        Just some random text to demonstrate how JSX can render HTML blocks with
+        JS functions
+      </p>
+    );
+  };
   return (
     <div className="container">
-      <button onClick={handleClick}>Clique aqui</button>
-      {clicked ? <p>You clicked!</p> : ""}
+      <div className="simple-event-click">
+        <button onClick={handleClick}>Click me</button>
+        {clicked ? <p>You clicked!</p> : ""}
+      </div>
+      <div className="simple-render-function">{renderText()}</div>
     </div>
   );
 };
