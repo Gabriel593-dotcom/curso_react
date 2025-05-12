ef37ebf8fbe1ad203dfc216919a6faa9d8c1c486
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-05-12 20:30:36 -0300

    AVANCANDO_EM_REACT/intro Fragment

---
+++ Curso React/AVANCANDO_EM_REACT/intro-fragment/src/components/FragmentEx.jsx
@@ -0,0 +1,13 @@
+const FragmentEx = () => {
+  return (
+    <>
+      <h1>Exemplo Fragment</h1>
+      <h4>Esse é um exemplo de fragment</h4>
+      <p>
+        O fragment elimina a necessidade de adicionar um elemento pai que não
+        tem nenhuma função dentro do DOM. Ele permite que o bloco HTML dentro
+        dele seja adicionado ao elemento que está chamando o componente.
+      </p>
+    </>
+  );
+};
