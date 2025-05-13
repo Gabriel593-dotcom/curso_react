de50cfc7bfaeec5e1376ef9be77a53ffb2e14aa1
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-05-13 20:47:32 -0300

    CSS_COM_REACT/intro css de component

---
+++ Curso React/CSS_COM_REACT/css-de-componentes/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CssExample from "./components/CssExample";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Css global</h1>
+      <p>Esse paragrado é do global</p>
+      <CssExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso React/CSS_COM_REACT/css-de-componentes/src/components/CssExample.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CssExample.css";
+
+const CssExample = () => {
+  return (
+    <div>
+      <h1>Css de component</h1>
+      <p>Esse é o paragrado do component</p>
+      <p className="my-comp-custom">Css custom do component</p>
+    </div>
+  );
+};
+
+export default CssExample;
