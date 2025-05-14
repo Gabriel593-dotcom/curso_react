c7090150786d8d2ef94d490cbf6513c36d98a28f
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-05-13 21:26:15 -0300

    CSS_COM_REACT/module css

---
+++ Curso React/CSS_COM_REACT/css-modules/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ModuleCss from "./components/ModuleCss";
+
+function App() {
+  return (
+    <div className="App">
+      <ModuleCss />
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso React/CSS_COM_REACT/css-modules/src/components/ModuleCss.jsx
@@ -0,0 +1,11 @@
+import styles from "./Style.module.css";
+
+const ModuleCss = () => {
+  return (
+    <div>
+      <h1 className={styles.my_title}>this is a example using module CSS</h1>
+    </div>
+  );
+};
+
+export default ModuleCss;
