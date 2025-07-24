5cdc5030b87e7ef32c36c1827d9fe9c099710949
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-07-23 21:12:34 -0300

    SECRET WORD - finalizando tela inicial

---
+++ Curso React/secret-word/src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Style from "./StartScreen.css";
+
+function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>clique no botão abaixo para iniciar o jogo</p>
+      <button>Iniciar</button>
+    </div>
+  );
+}
+
+export default StartScreen;
