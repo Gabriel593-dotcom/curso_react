c4ef7ef084ce100f3de91cac1ff00edadc091057
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2025-08-14 22:57:37 -0300

    REQUISICOES_HTTP/Resgatando dados com React

--- Curso React/REQUISICOES_HTTP/backend/src/App.js
+++ Curso React/REQUISICOES_HTTP/backend/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const baseUrl = 'http://localhost:3001/products';
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch(baseUrl);
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
-     <h1>Products List</h1>
+      <h1>Products List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
